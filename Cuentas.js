b74277bb13fb4b362e9c51230c74669ed04c3683
jlurgo <jlurgo@gmail.com>
2015-10-08 21:41:24 -0300

    bla

--- Cuentas.js
+++ Cuentas.js
@@ -1,13 +1,11 @@
 $(function(){
 	$('#selector_producto').select2({placeholder:"Producto", data: Repositorio.productos().map(function(prod){return {id:prod.id, text:prod.nombre}})});
-	$('#selector_caja_origen').select2({placeholder:"Origen", data: Repositorio.cuentas().map(function(cuenta){return {id:cuenta.id, text:cuenta.nombre}})});
-	$('#selector_caja_destino').select2({placeholder:"Destino", data: Repositorio.cuentas().map(function(cuenta){return {id:cuenta.id, text:cuenta.nombre}})});
+	$('#selector_caja').select2({placeholder:"Destino", data: Repositorio.cajas().map(function(caja){return {id:caja.id, text:caja.owner.nombre + " - " + caja.nombre}})});
 	$("#btn_agregar_movimiento").click(function(){
 		Repositorio.agregarMovimiento({
 			producto: parseInt($('#selector_producto').val()),
 			cantidad: parseInt($('#txt_cantidad').val()),
-			origen: parseInt($('#selector_caja_origen').val()),
-			destino: parseInt($('#selector_caja_destino').val())
+			origen: parseInt($('#selector_caja').val()),
 		});
 		VistaMovimientos.dibujar();
 	});
@@ -17,12 +15,11 @@ $(function(){
 var VistaMovimientos = {
 	dibujar: function(){
 		$("#movimientos").empty();
-		_.forEach(Repositorio.movimientos(), function(mov){
+		_.forEach(Repositorio.buscarMovimientos(), function(mov){
 			var vista_mov = $("#plantillas .vista_movimiento").clone();
 			vista_mov.find("#producto").text(mov.producto.nombre);
 			vista_mov.find("#cantidad").text(mov.cantidad);
-			vista_mov.find("#origen").text(mov.origen.nombre);
-			vista_mov.find("#destino").text(mov.destino.nombre);
+			vista_mov.find("#caja").text(mov.caja.owner.nombre + " - "  + mov.caja.nombre);
 			$("#movimientos").append(vista_mov);
 		});
 	}
--- Repositorio.js
+++ Repositorio.js
@@ -1,16 +1,22 @@
 var Repositorio = {
-	cuentas: function(){
-		return [
-			{id:1, nombre:"Caja Chica"},
-			{id:2, nombre:"Cuenta MercadoPago"},
-			{id:3, nombre:"Cuenta Banco Nacion"},
-			{id:4, nombre:"Charles"},
-			{id:5, nombre:"Jero"},
-			{id:6, nombre:"May"},
-			{id:7, nombre:"Bulonera GATA"},
-			{id:8, nombre:"Easy"},
-			{id:9, nombre:"Stock"}
-		];
+    _cajas: [
+        {id:1, owner: 1, nombre:"Caja Chica"},
+        {id:2, owner: 1, nombre:"MercadoPago"},
+        {id:3, owner: 1, nombre:"Banco Nacion"},
+        {id:4, owner: 2, nombre:"Stock"},
+        {id:5, owner: 3, nombre:"Stock"},
+        {id:6, owner: 4, nombre:"Stock"},
+        {id:7, owner: 5, nombre:"Stock"},
+        {id:8, owner: 6, nombre:"Stock"},
+        {id:9, owner: 1, nombre:"Stock"}
+    ],
+	cajas: function(){
+		var _this = this;
+		return this._cajas.map(function(c){
+            var caja = _.clone(c);
+            caja.owner = _.findWhere(_this.entes(), {id: c.owner});            
+			return caja;
+		});
 	},
 	productos: function(){
 		return [
@@ -22,19 +28,28 @@ var Repositorio = {
 			{id:5, nombre:"Tornillo Autoperforante Negro"}			
 		];
 	},
+    entes: function(){
+		return [
+			{id:1, nombre:"AmariSzi"},
+			{id:2, nombre:"Charles"},
+			{id:3, nombre:"Jero"},
+			{id:4, nombre:"Mayra"},
+			{id:5, nombre:"Bulonera GATA"},
+			{id:5, nombre:"Easy"}			
+		];
+	},
 	_movimientos: [
-		{id:1, fecha: "20/11", producto:1, cantidad: 150, origen: 1, destino: 7},
-		{id:2, fecha: "20/11", producto:5, cantidad: 100, origen: 7, destino: 9}
+		{id:1, fecha: "20/11", producto:1, cantidad: 150, caja: 1},
+		{id:2, fecha: "20/11", producto:5, cantidad: 100, caja: 7}
 	],
-	movimientos: function(){
+	buscarMovimientos: function(){
 		var _this = this;
 		return this._movimientos.map(function(mov){
 			return {
 				id:mov.id, 
 				producto: _.findWhere(_this.productos(), {id: mov.producto}),
 				cantidad: mov.cantidad,
-				origen: _.findWhere(_this.cuentas(), {id: mov.origen}),
-				destino: _.findWhere(_this.cuentas(), {id: mov.destino})				
+				caja: _.findWhere(_this.cajas(), {id: mov.caja})				
 			};
 		});
 	},
